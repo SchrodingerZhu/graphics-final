d64d8cd19ae0acc9a1cc4d9def31150816239925
SchrodingerZhu <i@zhuyi.fan>
2022-05-13 20:52:35 +0800

    feat: more
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <cmath>
-__attribute__((noinline)) void add(float (&a)[1024], float (&b)[1024]) {
-#pragma omp target map(tofrom:b[0:1024]) map(to:a[0:1024])
-#pragma omp teams distribute parallel for default(none) shared(a, b)
-    for (int i = 0; i < 1024; ++i) {
-        b[i] = ::sqrtf(a[i] * a[i] + b[i] * b[i]);
-    }
-}
+
 
 int main() {
-    float a[1024] = {1};
+    auto a = new float[1024];
+    auto b = new float[1024];
     std::fill(&a[0], &a[1024], 1);
-    float b[1024] = {1};
     std::fill(&b[0], &b[1024], 1);
-    for (int i = 0; i < 1000; ++i) {
-        add(a, b);
+#pragma omp target map(tofrom: b[0:1024]) map(to: a[0:1024])
+#pragma omp teams distribute parallel for default(none) shared(a, b)
+    for (int i = 0; i < 1024; ++i) {
+        b[i] = a[i] * a[i] + b[i] * b[i];
     }
-    for (float i: b) {
-        std::cout << i << std::endl;
+    float x = 0;
+#pragma omp target map(tofrom: x) map(to: b[0:1024])
+#pragma omp teams distribute reduction(+:x) default(none) shared(b)
+    for (int i = 0; i < 1024; ++i) {
+        x += b[i];
     }
+    std::cout << x << std::endl;
     return 0;
 }
