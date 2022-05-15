9e815c77144dd0c791a4361d35f5e0a3437ccfb6
SchrodingerZhu <i@zhuyi.fan>
2022-05-15 16:07:36 +0800

    stage
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

---
+++ include/hdr/hdr.hpp
@@ -0,0 +1,10 @@
+//
+// Created by schrodinger on 5/15/22.
+//
+
+#ifndef HDRPLUS_HDR_HPP
+#define HDRPLUS_HDR_HPP
+namespace hdr {
+
+}
+#endif //HDRPLUS_HDR_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
-#include <iostream>
-#include <cmath>
+#include <boost/system.hpp>
+#include <hdr10plus.h>
+#include <opencv2/photo.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+namespace hdr {
+    using namespace cv;
+
+    struct FramesWithMetadata {
+        std::vector<Mat> frames;
+        std::vector<double> exposure_time;
+    };
+
+    Mat hdr_pipeline(Mat& output, const FramesWithMetadata& data) {
+        Mat response;
+        auto calibrate = createCalibrateDebevec();
+        calibrate->process(data.frames, response, data.exposure_time);
+        Mat hdr;
+        auto merger = createMergeDebevec();
+        merger->process(data.frames, hdr, data.exposure_time, response);
+        Mat ldr;
+        auto tonemap = createTonemapReinhard();
+        tonemap->process(hdr, ldr);
+        return ldr;
+    }
 
 
-int main() {
-    auto a = new float[1024];
-    auto b = new float[1024];
-    std::fill(&a[0], &a[1024], 1);
-    std::fill(&b[0], &b[1024], 1);
-#pragma omp target map(tofrom: b[0:1024]) map(to: a[0:1024])
-#pragma omp teams distribute parallel for default(none) shared(a, b)
-    for (int i = 0; i < 1024; ++i) {
-        b[i] = a[i] * a[i] + b[i] * b[i];
-    }
-    float x = 0;
-#pragma omp target map(tofrom: x) map(to: b[0:1024])
-#pragma omp teams distribute reduction(+:x) default(none) shared(b)
-    for (int i = 0; i < 1024; ++i) {
-        x += b[i];
-    }
-    std::cout << x << std::endl;
-    return 0;
 }
+
+
