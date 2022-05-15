9fa605a445d2e43d0f3125cc1853597791c6874e
SchrodingerZhu <i@zhuyi.fan>
2022-05-15 16:15:09 +0800

    stage
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- include/hdr/hdr.hpp
+++ include/hdr/hdr.hpp
@@ -4,7 +4,19 @@
 
 #ifndef HDRPLUS_HDR_HPP
 #define HDRPLUS_HDR_HPP
-namespace hdr {
 
+#include <boost/system.hpp>
+#include <opencv2/photo.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+namespace hdr {
+    using namespace cv;
+
+    struct FramesWithMetadata {
+        std::vector<Mat> frames;
+        std::vector<float> exposure_time;
+    };
+
+    Mat hdr_pipeline(const FramesWithMetadata& data);
 }
 #endif //HDRPLUS_HDR_HPP
